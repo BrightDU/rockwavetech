844448dce4be296519c1b5e5f65176d63f097348
Bright Uto <mexcrew55@gmail.com>
2019-03-26 07:42:31 -0700

    activated the  Q library

--- Desktop/RockwaveApp/rockwavetech/server/mailer.js
+++ Desktop/RockwaveApp/rockwavetech/server/mailer.js
@@ -2,30 +2,30 @@ require("dotenv").config();
 var nodeMailer = require("nodemailer");
 var emailTemplate = require("email-templates");
 var sendMailTransport = require("nodemailer-smtp-transport");
-
+var Q = require('q');
 
 module.exports = {
     _template: null,
     _transport: null,
 
     init: function(config){
-        //var d = Q.defer();
+        var d = Q.defer();
 
         new emailTemplate(config.emailTplsDir, function(err, template){
             if(err){
-                return reject(err);
+                return d.reject(err);
             }
 
             this._template = template;
             this._transport = nodeMailer.createTransport(process.env.SMTP_USERNAME + ':' + process.env.SMTP_PASSWORD + '@smtp.gmail.com');
-            return resolve();
+            return d.resolve();
         }.bind(this));
 
-        return promise;
+        return d.promise;
     },
 
     send: function (from, to, subject, text, html) {
-        //var d = Q.defer();
+        var d = Q.defer();
         var params = {
           from: from, 
           to: to,
@@ -40,33 +40,33 @@ module.exports = {
         this._transport.sendMail(params, function (err, res) {
           if (err) {
             console.error(err);
-            return  reject(err);
+            return  d.reject(err);
           } else {
-            return  resolve(res);
+            return  d.resolve(res);
           }
         });
      
-        return promise;
+       return d.promise;
     },
 
     sendMail: function (from, to, subject, tplName, locals) {
-        //var d = Q.defer();
+        var d = Q.defer();
         var self = this;
         this.init({ emailTplsDir: "email-templates" }).then(function () {
           this._template(tplName, locals, function (err, html, text) {
             if (err) {
               console.error(err);
-              return reject(err);
+              return d.reject(err);
             }
      
             self.send(from, to, subject, text, html)
               .then(function (res) {
-                return resolve(res);
+                return d.resolve(res);
               });
           });
         }.bind(this));
      
-        return promise;
+        return d.promise;
     }
      
 };
